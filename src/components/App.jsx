ac16727622135af425a6eed98f5fa5eb96cf46c0
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2024-01-11 11:16:11 +0200

    searchbar add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import Searchbar from './components/Searchbar';
+import ImageGallery from './components/ImageGallery';
+import Button from './components/Button';
+import Loader from './components/Loader';
+import Modal from './components/Modal';
+import styles from './styles.css';
+
+const apiKey = '41213027-c6be3d4375fb01bb774365854';
+
 export const App = () => {
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [modalImage, setModalImage] = useState('');
+
+  useEffect(() => {
+    if (!query) return;
+
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `https://pixabay.com/api/?q=${query}&page=${page}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=12`
+        );
+        const data = await response.json();
+        setImages((prevImages) => [...prevImages, ...data.hits]);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [query, page]);
+
+  const handleSearch = (searchQuery) => {
+    setQuery(searchQuery);
+    setPage(1);
+    setImages([]);
+  };
+
+  const loadMoreImages = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  const openModal = (imageURL) => {
+    setShowModal(true);
+    setModalImage(imageURL);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setModalImage('');
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Searchbar onSubmit={handleSearch} />
+      <ImageGallery images={images} onImageClick={openModal} />
+      {loading && <Loader />}
+      {images.length > 0 && !loading && <Button onLoadMore={loadMoreImages} />}
+      {showModal && <Modal imageURL={modalImage} onClose={closeModal} />}
     </div>
   );
 };
+
+export default App;
