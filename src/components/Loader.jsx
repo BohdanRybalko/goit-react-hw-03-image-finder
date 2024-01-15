4157905da77e302bbb0832035ae3a5c0b9810a12
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2024-01-15 11:04:14 +0200

    fix loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import * as LoaderSpinner from 'react-loader-spinner';
+import {Oval}from 'react-loader-spinner';
 import styles from '../styles.css';
 
 const Loader = () => (
   <div className={styles.Loader}>
-    <LoaderSpinner type="Oval" color="#00BFFF" height={80} width={80} />
+    <Oval  color="#00BFFF" height={80} width={80} />
   </div>
 );
 
