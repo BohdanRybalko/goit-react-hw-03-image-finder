902a0891b0e5b861699bcfaefdb5de2c8c65c49e
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2024-01-16 09:58:02 +0200

    save

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -3,15 +3,35 @@ import ImageGalleryItem from './ImageGalleryItem';
 import styles from '../styles.css';
 
 class ImageGallery extends Component {
+  state = {
+    showModal: false,
+    selectedImage: null,
+  };
+
+  handleImageClick = (selectedImage) => {
+    this.setState({ showModal: true, selectedImage });
+  };
+
+  handleCloseModal = () => {
+    this.setState({ showModal: false, selectedImage: null });
+  };
+
   render() {
-    const { images, onImageClick } = this.props;
+    const { images } = this.props;
+    const { showModal, selectedImage } = this.state;
 
     return (
-      <ul className={styles.ImageGallery}>
-        {images.map((image) => (
-          <ImageGalleryItem key={image.id} image={image} onClick={onImageClick} />
-        ))}
-      </ul>
+      <div>
+        <ul className={styles.ImageGallery}>
+          {images.map((image) => (
+            <ImageGalleryItem key={image.id} image={image} onClick={this.handleImageClick} />
+          ))}
+        </ul>
+
+        {showModal && selectedImage && (
+          <Modal imageURL={selectedImage} onClose={this.handleCloseModal} />
+        )}
+      </div>
     );
   }
 }
--- src/components/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import styles from '../styles.css';
 
 const ImageGalleryItem = ({ image, onClick }) => (
-  <li className={styles.ImageGalleryItem} onClick={() => onClick(image.largeImageURL)}>
-    <img src={image.webformatURL} alt={image.tags} className={styles.ImageGalleryItemImage} />
+  <li className={styles.ImageGalleryItem}>
+    <img
+      src={image.webformatURL}
+      alt={image.tags}
+      className={styles.ImageGalleryItemImage}
+      onClick={() => onClick(image.largeImageURL)}
+    />
   </li>
 );
 
