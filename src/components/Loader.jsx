a19587a399410dc83bb5d0da0b8b30fbcac8fecb
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2024-01-16 17:21:12 +0200

    fix loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,20 @@
-import {Oval}from 'react-loader-spinner';
-import styles from '../styles.css';
+import React from 'react';
+import styled from 'styled-components';
+import { Oval } from 'react-loader-spinner';
 
-const Loader = () => (
-  <div className={styles.Loader}>
-    <Oval  color="#00BFFF" height={80} width={80} />
-  </div>
-);
+const SpinnerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
+`;
+
+const Loader = () => {
+  return (
+    <SpinnerContainer>
+      <Oval type="ThreeDots" color="#3f51b5" height={50} width={50} />
+    </SpinnerContainer>
+  );
+};
 
 export default Loader;
